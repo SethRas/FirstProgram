28c0b4c57aefc063c02737b399ec20edd6493b5c
rasset69@gmail.com <rasset69@gmail.com>
2022-01-11 08:40:55 -0700

    Add Header

--- FirstProgram/FirstProgram.vb
+++ FirstProgram/FirstProgram.vb
@@ -1,4 +1,9 @@
-﻿Module FirstProgram
+﻿'Seth Rasmussen
+'RCET 0265
+'Spring2022
+'My First Program
+'https://github.com/SethRas/FirstProgram.git
+Module FirstProgram
 
     Sub Main()
         Console.WriteLine("Hello_World_This_is_a_String")
